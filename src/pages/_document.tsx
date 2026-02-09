8cd7787b1c799ed97699b97743d672a101ee8bba
Kalanithi B <Kalanithi@amwhiz.com>
2026-02-09 14:52:02 +0530

    feat: Add Zoho Embedded App SDK scripts for CRM and Zwidgets.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <Script src="https://static.zohocdn.com/zohocrm/v2.0/sdk/4.0.0/sdk.js" strategy="afterInteractive" />
+        <Script src="https://crm.zoho.com/crm/javascript/ZohoEmbededAppSDK.min.js" strategy="beforeInteractive" />
+        <Script src="https://live.zwidgets.com/js-sdk/1.2/ZohoEmbededAppSDK.min.js" strategy="beforeInteractive" />
       </Head>
       <body className="antialiased">
         <Main />
