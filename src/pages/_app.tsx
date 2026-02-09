6e4955ab508b8b746a80db2d36c62b91307f545e
Kalanithi B <Kalanithi@amwhiz.com>
2026-02-09 14:48:31 +0530

    feat: Add Zoho SDK scripts for integration.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Header from "@port/components/Header";
 import "@port/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import { useEffect } from "react";
@@ -16,6 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrimeReactProvider>
       {/* <Header /> */}
+      <Script src="https://static.zohocdn.com/zohocrm/v2.0/sdk/4.0.0/sdk.js" strategy="afterInteractive" />
+      <Script src="https://crm.zoho.com/crm/javascript/ZohoEmbededAppSDK.min.js" strategy="beforeInteractive" />
+      <Script src="https://live.zwidgets.com/js-sdk/1.2/ZohoEmbededAppSDK.min.js" strategy="beforeInteractive" />
       <Component {...pageProps} />
       {/* <Footer /> */}
     </PrimeReactProvider>
